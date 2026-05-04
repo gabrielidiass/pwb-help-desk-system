99d66a6227c0b95139b726fccc7f03234ac769e6
Gabrieli Dias <gabrielilocatellidias508@gmail.com>
2026-05-04 20:08:50 -0300

    Adding SSL

--- help-desk-system-api/src/database/index.js
+++ help-desk-system-api/src/database/index.js
@@ -3,6 +3,12 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,
 });
 
