73cf228ca44d24ffc05ee4f83344b1c46126be40
Gabrieli Dias <gabrielilocatellidias508@gmail.com>
2026-06-17 07:20:37 -0300

    erro de import

--- help-desk-system-api/src/controller/SegurancaController.js
+++ help-desk-system-api/src/controller/SegurancaController.js
@@ -2,7 +2,7 @@
 import jwt from "jsonwebtoken";
 
 import dotenv from "dotenv-safe";
-import { autenticaUsuarioDB } from '../../useCases/SegurancaUseCases.js';
+import { autenticaUsuarioDB } from '../useCases/SegurancaUseCases';
 dotenv.config();
 
 const login = async (request, response) => {
