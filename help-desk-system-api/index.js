9e4dcc44b6cc466cd08de92c287be47bc255288b
Gabrieli Dias <gabrielilocatellidias508@gmail.com>
2026-06-17 10:09:32 -0300

    erros

--- help-desk-system-api/index.js
+++ help-desk-system-api/index.js
@@ -11,10 +11,17 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); // permite receber JSON no body
 app.use(router);
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
 
 // Sincroniza o banco ao iniciar
 await sequelize.authenticate();
 await sequelize.sync({ alter: true });
 console.log("Banco conectado e tabelas sincronizadas!");
 
-app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`Servidor rodando na porta ${PORT}`)
+);
