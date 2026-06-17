dfd38de204f45f45636789e2bebd2e6b32e3f5fa
Gabrieli Dias <gabrielilocatellidias508@gmail.com>
2026-06-17 08:35:19 -0300

    Ajuste de exportação de modulo

--- help-desk-system-api/src/useCases/SegurancaUseCases.js
+++ help-desk-system-api/src/useCases/SegurancaUseCases.js
@@ -1,22 +1,33 @@
 import sequelize from "../database/index.js";
-import Usuario from '../models/Usuario.js';
+import Usuario from "../models/Usuario.js";
+import { QueryTypes } from "sequelize";
 
 const autenticaUsuarioDB = async (body) => {
-    try {           
-        const { email, senha } = body
-        const results = await sequelize.query(`SELECT * FROM usuarios WHERE email = :email AND senha = :senha`,
-        { replacements: { email, senha }, type: sequelize.QueryTypes.SELECT });
+  try {
+    const { email, senha } = body;
 
-        if (results.length == 0) {
-            throw "Usuário ou senha inválidos";
-        }
-        const usuario = results[0];
-        return new Usuario(usuario.email, usuario.tipo, usuario.telefone, usuario.nome);
-    } catch (err) {
-        throw "Erro ao autenticar o usuário: " + err;
-    }    
-}
+    const results = await sequelize.query(
+      `SELECT * FROM usuarios WHERE email = :email AND senha = :senha`,
+      {
+        replacements: { email, senha },
+        type: QueryTypes.SELECT,
+      }
+    );
 
-module.exports = {
-    autenticaUsuarioDB
-}
+    if (results.length === 0) {
+      throw new Error("Usuário ou senha inválidos");
+    }
+
+    const usuario = results[0];
+    return new Usuario(
+      usuario.email,
+      usuario.tipo,
+      usuario.telefone,
+      usuario.nome
+    );
+  } catch (err) {
+    throw new Error("Erro ao autenticar o usuário: " + err.message);
+  }
+};
+
+export { autenticaUsuarioDB };
